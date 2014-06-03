5d5cc92fa73d33e7e2341a885d697df095246e8a
b1012187 <b1012187@fun.ac.jp>
2014-06-03 15:40:50 +0900

    message用のテキストフィールドをViewController.hを接続した closed #2

--- pushmessage/ViewController.h
+++ pushmessage/ViewController.h
@@ -12,5 +12,6 @@
 @property (weak, nonatomic) IBOutlet UILabel *name;
 @property (weak, nonatomic) IBOutlet UITextField *namefield;
 @property (weak, nonatomic) IBOutlet UILabel *message;
+@property (weak, nonatomic) IBOutlet UITextField *messagefield;
 
 @end
